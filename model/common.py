8ad8fbf6eb67d84fc06225ceff661664302dc1ea
Marcin Pleban <pleban.marcin@gmail.com>
2018-10-22 17:50:20 +0200

    generate_random done

--- model/common.py
+++ model/common.py
@@ -16,9 +16,25 @@ def generate_random(table):
     Returns:
         string: Random and unique string
     """
+    def guess_upper_letter():
+        return random.choice('abcdefghijklmnopqrstuvwxyz').upper()
 
-    generated = ''
+    def guess_letter():
+        return random.choice('abcdefghijklmnopqrstuvwxyz')
 
-    # your code
+    def guess_int():
+        return random.randint(1, 9)
 
+    a = str(guess_letter())
+    b = str(guess_upper_letter())
+    c = str(guess_int())
+    d = str(guess_int())
+    e = str(guess_upper_letter())
+    f = str(guess_letter())
+    generated = a + b + c + d + e + f + '#&'
+
+    for lines in table:
+        if lines[0] == generated:
+            return generate_random(table)
+        
     return generated
